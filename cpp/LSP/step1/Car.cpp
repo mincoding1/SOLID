6c3d64078f1ab8da2b7a33e9f3f1f91262a42d29
mincoding <74457149+mincoding1@users.noreply.github.com>
2023-04-14 01:16:22 +0900

    Update Car.cpp

--- cpp/LSP/step1/Car.cpp
+++ cpp/LSP/step1/Car.cpp
@@ -5,6 +5,7 @@
 using namespace std;
 
 class Car : public Vehicle {
+protected:
     void ChangeGear(Gear gear) override {
         if (Gear::R == gear && GetGear() == Gear::D) {
             string exception_msg = string("Can't change to REVERSE gear when ") + GetGearName(gear) + string(" gear is engaged!");
