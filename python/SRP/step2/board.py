7a3197c3692891619e054957fbc1b7b887e2361c
mincoding <74457149+mincoding1@users.noreply.github.com>
2023-05-07 21:36:50 +0900

    Update board.py

--- python/SRP/step2/board.py
+++ python/SRP/step2/board.py
@@ -1,31 +1,31 @@
 class Board:
     def __init__(self) -> None:
         super().__init__()
-        self.spots = []
+        self.__spots = []
         for i in range(9):
-            self.spots.append(str(i))
+            self.__spots.append(str(i))
 
     def get_first_row(self) -> []:
         first_row = []
-        first_row.append(self.spots[0])
-        first_row.append(self.spots[1])
-        first_row.append(self.spots[2])
+        first_row.append(self.__spots[0])
+        first_row.append(self.__spots[1])
+        first_row.append(self.__spots[2])
         return first_row
 
     def get_second_row(self) -> []:
         second_row = []
-        second_row.append(self.spots[3])
-        second_row.append(self.spots[4])
-        second_row.append(self.spots[5])
+        second_row.append(self.__spots[3])
+        second_row.append(self.__spots[4])
+        second_row.append(self.__spots[5])
         return second_row
 
     def get_third_row(self) -> []:
         third_row = []
-        third_row.append(self.spots[6])
-        third_row.append(self.spots[7])
-        third_row.append(self.spots[8])
+        third_row.append(self.__spots[6])
+        third_row.append(self.__spots[7])
+        third_row.append(self.__spots[8])
         return third_row
 
     def display(self):
-        formatted_first_row = self.spots[0] + "|" + self.spots[1] + "|" + self.spots[2] + "\n" + self.spots[3] + "|" + self.spots[4] + "|" + self.spots[5] + "\n" + self.spots[6] + "|" + self.spots[7] + "|" + self.spots[8] + "\n"
+        formatted_first_row = self.__spots[0] + "|" + self.__spots[1] + "|" + self.__spots[2] + "\n" + self.__spots[3] + "|" + self.__spots[4] + "|" + self.__spots[5] + "\n" + self.__spots[6] + "|" + self.__spots[7] + "|" + self.__spots[8] + "\n"
         print(formatted_first_row)
