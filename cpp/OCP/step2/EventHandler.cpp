0f0d1403f92f93b0a408b284fe439b38cb3ffdd8
mincoding <74457149+mincoding1@users.noreply.github.com>
2023-05-26 11:33:00 +0900

    Update EventHandler.cpp

--- cpp/OCP/step2/EventHandler.cpp
+++ cpp/OCP/step2/EventHandler.cpp
@@ -8,23 +8,23 @@ public:
 
 	void changeDrivingMode(DrivingMode driving_mode) {
 		switch (driving_mode) {
-		case DrivingMode::kSport: {
-			vehicle_.SetPower(500);
-			vehicle_.SetSuspensionHeight(10);
-			break;
-		}
-		case DrivingMode::kComfort: {
-			vehicle_.SetPower(400);
-			vehicle_.SetSuspensionHeight(20);
-			break;
-		}
-		default: {
-			vehicle_.SetPower(400);
-			vehicle_.SetSuspensionHeight(20);
-			break;
-		}
-		// when we need to add another mode (e.g. ECONOMY)
-		// 2 classes will change DrivingMode and EventHandler.
+			case DrivingMode::kSport: {
+				vehicle_.SetPower(500);
+				vehicle_.SetSuspensionHeight(10);
+				break;
+			}
+			case DrivingMode::kComfort: {
+				vehicle_.SetPower(400);
+				vehicle_.SetSuspensionHeight(20);
+				break;
+			}
+			default: {
+				vehicle_.SetPower(400);
+				vehicle_.SetSuspensionHeight(20);
+				break;
+			}
+			// when we need to add another mode (e.g. ECONOMY)
+			// 2 classes will change DrivingMode and EventHandler.
 		}
 	}
 
